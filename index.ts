9fdcacfc9695b5fbdd394c8c92cfc2ad4ee6d8d3
brunorcajo <bruno@orcajo.com.br>
2020-05-21 16:48:51 -0300

    Article #1

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
+
 import { Observable } from 'rxjs';
+
+const fetchFake$ = new Observable((subscriber) => {
+  const fetch = setInterval(() => {
+    subscriber.next({
+      status: "success",
+      data: [
+        { name: "Bruno", lastName: "Orcajo" },
+        { name: "Igor", lastName: "Orcajo" },
+      ],
+    });
+
+    subscriber.complete();
+  }, 5000);
+
+  return () => {
+    clearInterval(fetch);
+  };
+});
+
+const observer = {
+  next: (data) => console.log(data),
+  error: (err) => console.log(err),
+  complete: () => console.log("completed")
+};
+
+const subscriber = fetchFake$.subscribe(observer);
